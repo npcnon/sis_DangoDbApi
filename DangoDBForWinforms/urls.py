3fabfa49bb7b0ffd5e3088c0cf35d8fecfa3fa62
non <afkmhafric@gmail.com>
2024-03-04 17:09:04 +0800

    modified the 2nd path on dangoforwinforms.urls

--- DangoDBForWinforms/urls.py
+++ DangoDBForWinforms/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('DangoDBApp.urls'))
+    path('api/', include('DangoDBApp.urls'))
 ]
