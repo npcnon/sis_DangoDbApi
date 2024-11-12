a2ce473d4a2d96b1c150d76eef35b3324de68874
npcnon <afkmhafric@gmail.com>
2024-11-12 17:12:11 +0800

    fixed root url

--- DangoDBForWinforms/urls.py
+++ DangoDBForWinforms/urls.py
@@ -5,11 +5,10 @@ from django.urls import path, include
 from .views import display_all_data
 
 urlpatterns = [
-    # path('', display_all_data, name='all_data'),
+    path('alldata/', display_all_data, name='all_data'),
     path('admin/', admin.site.urls),
     path('api/', include('DangoDBApp.urls')),
     path('api/', include('users.urls')),
     path('api/', include('FileApp.urls')),
-
 ]
 
