4d54dceb3df9ba29ec62d242d98180ebf59f5504
npcnon <afkmhafric@gmail.com>
2024-11-29 00:28:04 +0800

    fixed urls

--- FileApp/urls.py
+++ FileApp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import FileUploadView
 from .document_delete_view import DocumentDeleteView
 from .views import AdminDocumentView
-from .views import AdminContactView,AdminContactRetrieveView
 urlpatterns = [
     path('upload/', FileUploadView.as_view(), name='file-upload'),
     path('documents/', FileUploadView.as_view(), name='document-list'),
@@ -11,7 +10,5 @@ urlpatterns = [
     path('delete/', DocumentDeleteView.as_view(), name='file-upload'),
     path('admin-documents/', AdminDocumentView.as_view(), name='admin-documents'),
     path('admin-documents', AdminDocumentView.as_view(), name='admin-documents'),
-    path('post-help/', AdminContactView.as_view(), name='admin-documents'),
-    path('get-help', AdminContactRetrieveView.as_view(), name='admin-documents'),
 
 ]
